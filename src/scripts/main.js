4992ed56fae804120aae3f35d5b426acef9f051a
Yaroslav Grom <gromyarik3@gmail.com>
2026-01-26 18:13:36 +0100

    add index.html for GitHub Pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
+/* global Swiper */
 'use strict';
+
+window.addEventListener('load', () => {
+  const swiper = new Swiper('.work-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    speed: 800,
+    loop: false,
+
+    grabCursor: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+});
